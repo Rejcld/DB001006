8514bc89b5f4ca9865d8766f7c37c2f143095e3c
Rejcld <rejishs@yahoo.com>
2021-12-24 20:50:51 +0300

    16

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "compartment_ocid" { default = "ocid1.compartment.oc1..aaaaaaaasxt7qjw3
 variable "region" { default = "ap-mumbai-1" }
 variable "tenancy_ocid" { default = "ocid1.tenancy.oc1..aaaaaaaatkvbg2gt7ziju5ya2tezy7ljw7yxra6qtyx5ebf4rgh3gj4bfvwq" }
 variable "user_ocid" { default = "ocid1.user.oc1..aaaaaaaanao45uiqi5mar4huduir3c7lga2dakp7aufyds2r36wrbrxkqfcq" }
-variable "fingerprint" { default = "fa:97:8e:a8:e3:ac:82:6e:cf:5d:f6:28:e2:fd:f9:48" }
+variable "fingerprint" { default = "d6:18:f7:c9:1a:77:9b:05:a7:34:86:9a:a2:81:6d:97" }
 variable "private_key_path" { default ="/var/lib/jenkins/.oci/oci_api_key.pem" }
 
  
